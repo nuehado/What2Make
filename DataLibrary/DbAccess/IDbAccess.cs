dec3d816b4d1d315d25fe19c9709e1401661795c
nuehado <51039077+nuehado@users.noreply.github.com>
2021-03-24 16:59:26 -0400

    +added database access class and interface

---
+++ DataLibrary/DbAccess/IDbAccess.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataLibrary.DbAccess
+{
+    public interface IDbAccess
+    {
+        Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName);
+        Task<int> SaveData<U>(string storedProcedure, U parameters, string connectionStringName);
+    }
+}
---
+++ DataLibrary/DbAccess/SqlDb.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.DbAccess
+{
+    public class SqlDb : IDbAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDb(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<List<T>> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+
+                return rows.ToList();
+            }
+        }
+
+        public async Task<int> SaveData<U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                return await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
