38dcd7cae47f71551641c02daef1b299778aadb3
nuehado <51039077+nuehado@users.noreply.github.com>
2021-03-24 13:45:06 -0400

    +added ingredient search stored procedure

--- What2MakeDB/Scripts/StoredProcedureTests.sql
+++ What2MakeDB/Scripts/StoredProcedureTests.sql
@@ -44,3 +44,30 @@ begin
 	values (@RecipeId, @Id, @Quantity);
 
 end
+
+/*test searching by ingredient*/
+begin
+	
+	declare @Ingredient1 nvarchar(50) = 'cherry tomatoes';
+	declare @Ingredient2 nvarchar(50) = 'hot dog bun';
+	declare @Ingredient3 nvarchar(50) = 'KETCHUP';
+	declare @Ingredient4 nvarchar(50) = 'romain lettuce';
+	declare @Ingredient5 nvarchar(50) = 'HOT doG';
+
+	set nocount on;
+
+	with t as(
+	select r.Id, r.RecipeName, [Description], ROW_NUMBER() over(partition by RecipeName order by RecipeName) as Matches 
+	from dbo.Recipies r
+	inner join dbo.RecipiesIngredients ri on ri.RecipeId = r.Id
+	inner join dbo.Ingredients i on i.Id = ri.IngredientId
+	where i.IngredientName in (@Ingredient1, @Ingredient2, @Ingredient3, @Ingredient4, @Ingredient5)
+	)
+
+	select t.Id, t.RecipeName, t.[Description], t.Matches
+	from t
+	left outer join t [copy]
+	on t.RecipeName = [copy].RecipeName and t.Matches < [copy].Matches
+	where [copy].RecipeName is null
+	order by Matches desc
+end
---
+++ What2MakeDB/dbo/StoredProcedures/spRecipies_IngredientsSearch.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[spRecipies_IngredientsSearch]
+	@Ingredient1 nvarchar(50),
+	@Ingredient2 nvarchar(50),
+	@Ingredient3 nvarchar(50),
+	@Ingredient4 nvarchar(50),
+	@Ingredient5 nvarchar(50)
+AS
+begin
+	set nocount on;
+
+	with t as(
+	select r.Id, r.RecipeName, [Description], ROW_NUMBER() over(partition by RecipeName order by RecipeName) as Matches 
+	from dbo.Recipies r
+		inner join dbo.RecipiesIngredients ri on ri.RecipeId = r.Id
+		inner join dbo.Ingredients i on i.Id = ri.IngredientId
+		where i.IngredientName in (@Ingredient1, @Ingredient2, @Ingredient3, @Ingredient4, @Ingredient5)
+	)
+
+	select t.Id, t.RecipeName, t.[Description], t.Matches
+	from t
+		left outer join t [copy]
+		on t.RecipeName = [copy].RecipeName and t.Matches < [copy].Matches
+		where [copy].RecipeName is null
+	order by Matches desc
+end
