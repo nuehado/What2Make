54af5764799282cb4e67817498b0f0d7b7eacd9f
nuehado <51039077+nuehado@users.noreply.github.com>
2021-04-03 13:15:58 -0400

    +added scaffolding for search recipes by ingredients page and the data models it should need

---
+++ What2Make_RazorPages/Models/Read/RecipeSearchListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace What2Make_RazorPages.Models.Read
+{
+    public class RecipeSearchListModel
+    {
+        public List<RecipeSearchModel> Recipes { get; set; }
+    }
+}
---
+++ What2Make_RazorPages/Models/Read/RecipeSearchModel.cs
@@ -0,0 +1,10 @@
+﻿namespace What2Make_RazorPages.Models.Read
+{
+    public class RecipeSearchModel
+    {
+        public int Id { get; set; }
+        public string RecipeName { get; set; }
+        public string Description { get; set; }
+        public int Matches { get; set; }
+    }
+}
